b36d4e751b4717378bf333419ffb1617c3a47c2d
Govind Tiwari <108tiwari.g@gmail.com>
2025-07-09 10:50:13 +0530

    Update Header.tsx

--- Components/PagesComponents/Header.tsx
+++ Components/PagesComponents/Header.tsx
@@ -22,7 +22,7 @@ function Header() {
                     <a href='https://github.com/govindti' target="_blank" rel="noopener noreferrer" className="text-slate-200 hover:text-slate-400 transition duration-300 ease-in-out">
                         <GitHubIcon />
                     </a>
-                    <a href='https://drive.google.com/file/d/1bzFU4zHAoXvoNiUr4up2JfKwOV5OzKg4/view?usp=sharing' target="_blank" rel="noopener noreferrer" className="text-slate-200 hover:text-slate-400 transition duration-300 ease-in-out">
+                    <a href='https://drive.google.com/file/d/1E2GgHVMGnMLRjPHYuY0Yao-4-2RH26VL/view' target="_blank" rel="noopener noreferrer" className="text-slate-200 hover:text-slate-400 transition duration-300 ease-in-out">
                         <InsertDriveFileIcon />
                     </a>
                 </div>
