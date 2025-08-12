7d6b0f610d5a24e9b50c96d2d8ba50266439bfc3
govindti <108tiwari.g@gmail.com>
2025-08-12 10:55:49 +0530

    Add additional skills to SkillsTools component

--- Components/PagesComponents/SkillsTools.tsx
+++ Components/PagesComponents/SkillsTools.tsx
@@ -7,6 +7,7 @@ function SkillsTools() {
                 <h2 className='text-xl font-medium text-slate-100 '>Tools & Skills</h2>
             </div>
             <div className='flex flex-wrap gap-2 mt-2 mx-auto max-w-2xl md:px-4 '>
+                <div className='bg-slate-100 text-slate-800 p-1 border rounded shadow text-xs font-semibold'>Python</div>
                 <div className='bg-slate-100 text-slate-800 p-1 border rounded shadow text-xs font-semibold'>JavaScript</div>
                 <div className='bg-slate-100 text-slate-800 p-1 border rounded shadow text-xs font-semibold'>TypeScript</div>
                 <div className='bg-slate-100 text-slate-800 p-1 border rounded shadow text-xs font-semibold'>TailwindCSS</div>
@@ -15,6 +16,8 @@ function SkillsTools() {
                 <div className='bg-slate-100 text-slate-800 p-1 border rounded shadow text-xs font-semibold'>Express.JS</div>
                 <div className='bg-slate-100 text-slate-800 p-1 border rounded shadow text-xs font-semibold'>MongoDB</div>
                 <div className='bg-slate-100 text-slate-800 p-1 border rounded shadow text-xs font-semibold'>Next.JS</div>
+                <div className='bg-slate-100 text-slate-800 p-1 border rounded shadow text-xs font-semibold'>PostgreSQL</div>
+                <div className='bg-slate-100 text-slate-800 p-1 border rounded shadow text-xs font-semibold'>Prisma ORM</div>
                 <div className='bg-slate-100 text-slate-800 p-1 border rounded shadow text-xs font-semibold'>NextAuth</div>
                 <div className='bg-slate-100 text-slate-800 p-1 border rounded shadow text-xs font-semibold'>Docker</div>
                 <div className='bg-slate-100 text-slate-800 p-1 border rounded shadow text-xs font-semibold'>CI/CD</div>
